fb10a4d6000f66ecc846fe8086eaf85126dd7dc1
denis0001 <francofl123@gmail.com>
2017-03-17 12:31:03 -0300

    Add files via upload

--- Clase_02/ejercicio/main.c
+++ Clase_02/ejercicio/main.c
@@ -1,64 +1,115 @@
-/******************************************************************
-* Programa: Ejemplo Clase 2
-*
-* Objetivo:
-*   -Realizar un programa que solicite cinco números e imprima por pantalla el
-*    promedio, el máximo y el mínimo.
-*
-* Aspectos a destacar:
-*   -El uso de bucles para pedir un número tras otro
-*   -El uso de acumuladores, se tiene que guardar la suma para luego hacer
-*    el promedio
-*   -El uso del if para preguntar si el numero ingresado es mayor o menor
-*    para guardar los máximos y mínimos
-*
-* Version: 0.1 del 29 diciembre de 2015
-* Autor: Mauricio Dávila
-* Revisión: Ernesto Gigliotti
-*
-* *******************************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
+#include <conio.h>
 
 int main()
 {
-    int maximo;
-    int minimo;
-    int acumulador;
-    int auxiliarInt;
+    int numero;
+    int cantPares=0;
+    int cantImpares=0;
+    int sumImpares=0;
+    int contTodo;
+    int sumTodo=0;
+    float promedio;
+    int numMayor;
+    int numMenorImpar=0;
+    int cont315=0;
+    int cont1629=0;
+    int cont30plus=0;
+    char respuesta='s';
     int i;
+    //int flag=0;
 
-    // Solicito el primer número el cual por ser el primero sera tanto el
-    // maximo como el minimo y el primer numero a ser acumulado
-    printf("Ingrese un numero: ");
-    scanf("%d",&auxiliarInt);
-    maximo = auxiliarInt;
-    minimo = auxiliarInt;
-    acumulador = auxiliarInt;
-
-    // Solicito el resto de los numeros
-    for(i=0;i<4;i++)
+    while(respuesta=='s')
     {
-        printf("Ingrese un numero: ");
-        scanf("%d",&auxiliarInt);
-        acumulador = acumulador + auxiliarInt;
+        printf("Ingrese numero positivo:");
+        scanf("d", &numero);
 
-        if(auxiliarInt > maximo)
+        while(numero<1)
         {
-            maximo = auxiliarInt;
+            printf("Ingrese numero positivo:");
+            scanf("d", &numero);
         }
 
-        if(auxiliarInt < minimo)
+        if(numero%2==0)
         {
-            minimo = auxiliarInt;
+            cantPares++;
+
         }
+        if(numero%2==1)
+        {
+            cantImpares++;
+            sumImpares=+numero;
+        }
+
+        if (contTodo==0)
+        {
+            numMayor=numero;
+        }
+        else
+        {
+            if(numMayor<numero)
+            {
+                numMayor=numero;
+            }
+        }
+
+        /*if (flag==0)
+        {
+            max=numero;
+            flag=1;
+        }
+        else
+        {
+            if (num>max)
+            {
+                max=num;
+            }
+        */
+
+        //if(flag==0 || numero>max)
+
+        }
+
+
+
+        if(numero>2 && numero<16)
+        {
+            cont315++;
+        }
+        if(numero>15 && numero<30)
+        {
+            cont1629++;
+        }
+        if (numero>29)
+        {
+            cont30plus++;
+        }
+
+        contTodo++;
+        sumTodo+=numero;
+
+        printf("¿Desea seguir ingresando numeros? s/n");
+        respuesta=tolower(getche());
+
     }
 
-    printf("El maximo es: %i\n",maximo);
-    printf("El minimo es: %i\n",minimo);
-    printf("El promedio es: %f\n", ((float)acumulador) / 5);
-    return 0;
+    /*for(i=sumImpares;i>0;sumImpares--)
+    {
+        printf("%d", i)
+    }
+    */
+    promedio=(float)contTodo/sumTodo;
 
+    printf("\nLa cantidad de numeros pares es: %d", cantPares);
+    printf("\nLa cantidad de numeros impares es: %d", cantImpares);
+    printf("\nLa sumatoria de todos los numeros impares es: %d",sumImpares);
+    printf("\nEl promedio de todos los numeros es: %f", promedio);
+    printf("\nEl numero mayor ingresado es: %d", numMayor);
+    printf("\nEl numero impar menor ingresado es: %d",numMenorImpar);
+    printf("\nEntre 3-15 hay: %d entre 16-29: %d y 30 o mas: %d", cont315,cont1629,cont30plus);
+
+
+    return 0;
 }
