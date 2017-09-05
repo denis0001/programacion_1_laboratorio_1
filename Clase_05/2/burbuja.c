bff1a78e7f272337c47479798333026666e0fce7
denis0001 <francofl123@gmail.com>
2017-09-05 12:10:39 -0300

    Add files via upload

---
+++ Clase_05/2/burbuja.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int vector[4]={6,8,4,7};
+    int i,j,k;
+   /* for (i=0;i<3;i++)
+    {
+        for(j=i+1;j<4;j++)
+        {
+            if(vector[i]>vector[j])
+            {
+                i=k;
+                j=i;
+                k=j;
+            }
+        }
+    }
+*/
+    for (i=0;i>4;i++)
+    {
+        printf("%d",vector);
+    }
+    return 0;
+}
