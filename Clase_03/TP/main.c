9d33bb2f1c340952e9889c0836a396ac8c8785cf
denis0001 <francofl123@gmail.com>
2018-08-29 12:24:44 -0300

    Add files via upload

---
+++ Clase_03/TP/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+
+
+int main()
+{
+    int opcion;
+    float x,y;
+
+    do{
+        printf("MENU");
+        printf("\n\n1.Ingresar 1er operando (A= %.0f)",x);
+        printf("\n2.Ingresar 2do operando (A= %.0f)",y);
+        printf("\n3.")
+
+        printf("\n\nSelecciones opcion:");
+        scanf("%d", opcion);
+
+        switch(opcion){
+            case 1:
+
+                break
+
+
+
+
+
+            default:
+
+        }
+
+    }while(opcion!=5);
+
+
+    return 0;
+}
