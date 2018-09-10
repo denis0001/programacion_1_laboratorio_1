cfb57e200e10e50448bc5726b51764b15937737f
denis0001 <francofl123@gmail.com>
2018-09-10 12:23:14 -0300

    Add files via upload

---
+++ Clase_06/cadenas/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+    char nombre[] = "FEDERICO";
+
+    //char copia[9];
+    strlwr(nombre);
+    for (int i=0; nombre)
+
+    }
+
+    //strcpy(copia,nombre);
+    printf("%s\n",nombre);
+    //printf("%s", copia);
+    return 0;
+}
